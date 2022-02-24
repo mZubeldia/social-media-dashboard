b2c296ffd6c4d1cb54214456c34b9a5503c2efe6
María Zubeldia <mzubeldia09@gmail.com>
2022-02-24 18:24:34 +0100

    add font styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <header>
-        <div>
+      <header className="header">
+        <div className="title_and_followers_wrapper">
           <h1>Social Media Dashboard</h1>
-          Total Followers: 23,004
+          <span className="bold">Total Followers: 23,004</span>
         </div>
-        <div>Dark Mode</div>
+        <div className="theme_color">
+          <span className="bold">Dark Mode</span>
+          <button>boton</button>
+        </div>
       </header>
     </div>
   );
