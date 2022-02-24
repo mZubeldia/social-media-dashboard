7e36d5325a384c77c9b8a1c9a434a16da0375a00
María Zubeldia <mzubeldia09@gmail.com>
2022-02-24 17:01:03 +0100

    create react app and clean it

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Social Media Dashboard</h1>
+          Total Followers: 23,004
+        </div>
+        <div>Dark Mode</div>
+      </header>
+
+      <main>
+        <section>
+          <article>@nathanf 1987 Followers 12 Today</article>
+          <article>@nathanf 1044 Followers 99 Today</article>
+          <article>@realnathanf 11k Followers 1099 Today</article>
+          <article>Nathan F. 8239 Subscribers 144 Today</article>
+        </section>
+        <section>
+          Overview - Today
+          <article>Page Views 87 3%</article>
+          <article>Likes 52 2%</article>
+          <article>Likes 5462 2257%</article>
+          <article>Profile Views 52k 1375%</article>
+          <article>Retweets 117 303%</article>
+          <article>Likes 507 553%</article>
+          <article>Likes 107 19%</article>
+          <article>Total Views 1407 12%</article>
+        </section>
+      </main>
+
+      <footer>
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">mZubeldia</a>.
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
